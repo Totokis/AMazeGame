0b55d0c91c7a1b359659fe1d5bfcc82d5240cfde
Paweł Janusz <trollmenjp@gmail.com>
2024-03-13 14:19:43 +0100

    refactor(Player): Input method

--- Assets/Scripts/CMPlayer.cs
+++ Assets/Scripts/CMPlayer.cs
@@ -3,33 +3,13 @@ using UnityEngine;
 public class CMPlayer : MonoBehaviour
 {
     [SerializeField] private float moveSpeed;
+    [SerializeField] private GameInput gameInput;
+
     private bool _isWalking;
 
     private void Update()
     {
-        var inputVector = new Vector2(0, 0);
-        if (Input.GetKey(KeyCode.W))
-        {
-            inputVector.y += 1;
-        }
-
-        if (Input.GetKey(KeyCode.S))
-        {
-            inputVector.y -= 1;
-        }
-
-        if (Input.GetKey(KeyCode.D))
-        {
-            inputVector.x += 1;
-        }
-
-        if (Input.GetKey(KeyCode.A))
-        {
-            inputVector.x -= 1;
-        }
-
-        inputVector = inputVector.normalized;
-
+        var inputVector = gameInput.GetMovementVectorNormalized();
         var moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
 
         _isWalking = moveDir != Vector3.zero;
